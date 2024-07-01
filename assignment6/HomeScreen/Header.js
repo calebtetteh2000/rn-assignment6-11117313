c18210462909bdd1f36dcbe625ef2a040b83d518
calebtetteh2000 <calebtetteh65@gmail.com>
2024-07-01 19:58:38 +0000

    App Created

---
+++ assignment6/HomeScreen/Header.js
@@ -0,0 +1,9 @@
+import React from "react";
+import { StyleSheet, View, Text, Image, TouchableOpacity } from "react-native";
+import { useNavigation } from "@react-navigation/native";
+
+function Header(){
+    return(
+        <Image></Image>
+    )
+}
---
+++ assignment6/HomeScreen/HomeScreen.js
@@ -0,0 +1,15 @@
+import React from "react";
+import { StyleSheet, View, ScrollView } from "react-native";
+import Header from "./Header"
+
+return(
+    <View style={styles.container}>
+        <ScrollView>
+            <Header />
+        </ScrollView>
+    </View>
+)
+
+const styles = StyleSheet.create({
+
+})
